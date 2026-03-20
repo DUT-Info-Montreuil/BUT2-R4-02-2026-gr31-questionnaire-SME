8e7263479319882ef8072dc007efb74ae7e49b27
CodinDiego <mangadodiego@gmail.com>
2026-03-20 11:37:39 +0100

    Changement iQuestionnaire

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr31/services/interfaces/Iquestionnaire.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr31/services/interfaces/Iquestionnaire.java
@@ -2,13 +2,15 @@ package universite_Paris8.iut.qdev.tp2026.gr31.services.interfaces;
 
 import universite_Paris8.iut.qdev.tp2026.gr31.commons.dtos.QuestionDTO;
 import universite_Paris8.iut.qdev.tp2026.gr31.commons.dtos.QuestionnaireDTO;
+import universite_Paris8.iut.qdev.tp2026.gr31.commons.enums.Langue;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public interface Iquestionnaire {
         public ArrayList<QuestionnaireDTO> fournirListeQuestionnaire ();
-        public void chargerFichier();
-        public QuestionnaireDTO fournirUnQuestionnaire();
+        public boolean chargerFichier(File file);
+        public QuestionnaireDTO fournirUnQuestionnaire(int dificulte, Langue langue,String libelle);
         public void majStatQuestions();
         public QuestionDTO fournirStatQuestion();
 
